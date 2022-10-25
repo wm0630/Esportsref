a7c063952f498008c58b287266715d257957e3d1
DevBeast630 <104744139+DevBeast630@users.noreply.github.com>
2022-10-25 09:37:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require('express')();
-const server = require('http').Server(app);
+const app = require('express')()
+const server = require('http').Server(app)
 const io = require('socket.io')(server);
 const next = require('next');
 
@@ -31,7 +31,9 @@ const ChatHistory = mongoose.model('ChatHistory', chatHistorySchema);
 let users = [];
 const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
+  
   room = room.trim().toLowerCase();
+  
   const existingUser = users.find((user) => user.room === room && user.name === name);
   if (!name || !room) return { error: 'Username and room are required.' };
   if (existingUser) return { error: 'Username is taken.' };
